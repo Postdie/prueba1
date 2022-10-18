f4cc95a2f05197fe6413848d35a43a8c42e39f8c
Postdie <fernandezdiego845@gmail.com>
2022-10-18 03:23:25 -0500

    Pruebacommit1

---
+++ app/Http/Controllers/diegoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class diegoController extends Controller
+{
+    //movephp
+    public function index(){
+        return view('welcome');
+    }
+    public function prueba1guardar(request $request)
+    (){
+        
+    }
+}
